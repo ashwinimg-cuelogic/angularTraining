20452dab60f39ce606d83bb758420f3e283d109f
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-02-18 15:37:27 +0530

    #comment added navigation links to the application

--- js/controllers.js
+++ js/controllers.js
@@ -10,6 +10,8 @@ flightController.controller('ListController', ["$scope", "$http", function($scop
 flightController.controller('DetailsController', ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams){
     $http.get('js/data.json').success(function(data) {
         $scope.flights = data;
+        $scope.prevItem = ($routeParams.itemId > 0) ? Number($routeParams.itemId) - 1 : $scope.flights.length - 1;
+        $scope.nextItem = ($routeParams.itemId < ($scope.flights.length - 1)) ? Number($routeParams.itemId) + 1 : 0;
         $scope.whichFlight = $routeParams.itemId;
     });
 }]);
