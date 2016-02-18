cbfa5a44d61705b39caaae3f8193556adb622c92
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-02-18 14:28:27 +0530

    #comment used filters

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,8 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller('MyController', function MyController($scope, $http){
+myApp.controller('MyController', ["$scope", "$http", function($scope, $http){
     $http.get('js/data.json').success(function(data) {
         $scope.flights = data;
+        $scope.flightOrder = "number";
     });
-});
+}]);
