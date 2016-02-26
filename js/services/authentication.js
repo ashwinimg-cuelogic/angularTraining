c89f71ac7c796dba1078859ca93729a069c8a58e
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-02-26 19:36:33 +0530

    #comment added logged in user to the rootscope

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,7 +1,17 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL',
-    function($rootScope, $firebaseAuth, $location, FIREBASE_URL){
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$firebaseObject', '$location', 'FIREBASE_URL',
+    function($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL){
         var ref = new Firebase(FIREBASE_URL);
         var auth = $firebaseAuth(ref);
+
+        auth.$onAuth(function(authUser){
+            if (authUser) {
+                var userRef = new Firebase(FIREBASE_URL+ 'users/' + authUser.uid);
+                var userObj = $firebaseObject(userRef);
+                $rootScope.currentUser = userObj;
+            } else {
+                $rootScope.currentUser = '';
+            }
+        });
 
         return {
             login: function(user) {
